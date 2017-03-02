0f6e320a5863238de37c0b5c72967b3dd9e417a2
jonschlinkert <jon.schlinkert@sellside.com>
2017-03-02 14:30:13 -0500

    clean up, use make-iterator, lint

--- test.js
+++ test.js
@@ -7,21 +7,50 @@
 
 'use strict';
 
-var should = require('should');
+var assert = require('assert');
 var reduce = require('./');
 
-describe('.reduce()', function () {
-  it('should return the accumulated result after running each property in the obj through the callback.', function () {
+describe('.reduce()', function() {
+  it('should use an initializer for the accumulator', function() {
     var a = {a: 'foo', b: 'bar', c: {}};
-    var obj = reduce(a, function (acc, value, key, orig) {
+    var init = {};
+
+    reduce(a, function(acc, value, key, orig) {
       if (typeof value === 'object') {
-        acc[key] = {what: 'huh?'};
+        acc[key] = {what: 'who?'};
       } else {
-        acc[key] = value.toUpperCase(); // why?
+        acc[key] = value.toUpperCase();
+      }
+      return acc;
+    }, init);
+
+    assert.deepEqual(init, {a: 'FOO', b: 'BAR', c: {what: 'who?'}});
+  });
+
+  it('should take a context as the last argument', function() {
+    var a = {a: 'foo', b: 'bar', c: {}};
+    var ctx = {a: 'x', b: 'y', c: 'z'};
+    var init = {};
+
+    reduce(a, function(acc, value, key, orig) {
+      acc[key] = this[key];
+      return acc;
+    }, init, ctx);
+
+    assert.deepEqual(init, {a: 'x', b: 'y', c: 'z'});
+  });
+
+  it('should run each property in the obj through the callback.', function() {
+    var a = {a: 'foo', b: 'bar', c: {}};
+    var obj = reduce(a, function(acc, value, key, orig) {
+      if (typeof value === 'object') {
+        acc[key] = {what: 'who?'};
+      } else {
+        acc[key] = value.toUpperCase();
       }
       return acc;
     }, {});
 
-    obj.should.eql({a: 'FOO', b: 'BAR', c: {what: 'huh?'}});
+    assert.deepEqual(obj, {a: 'FOO', b: 'BAR', c: {what: 'who?'}});
   });
 });
